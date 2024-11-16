5faed19be7f7bb21271be7bf5bd9afea79ffd7d6
Mariany P. <marianypinkston@gmail.com>
2024-11-15 20:23:54 -0500

    Changed repairing broken arrow plop item on fletching table

--- src/main/java/dev/mariany/martweaks/item/custom/BrokenArrowItem.java
+++ src/main/java/dev/mariany/martweaks/item/custom/BrokenArrowItem.java
@@ -1,20 +1,23 @@
 package dev.mariany.martweaks.item.custom;
 
 import dev.mariany.martweaks.MarTweaks;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FletchingTableBlock;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
@@ -28,16 +31,14 @@ public class BrokenArrowItem extends Item {
         World world = context.getWorld();
         Random random = world.getRandom();
         PlayerEntity player = context.getPlayer();
-        Hand hand = context.getHand();
         ItemStack stack = context.getStack();
         BlockPos pos = context.getBlockPos();
         BlockState blockState = world.getBlockState(pos);
 
         if (player != null) {
-            if (blockState.getBlock() instanceof FletchingTableBlock fletchingTable) {
+            if (blockState.getBlock() instanceof FletchingTableBlock) {
                 if (world instanceof ServerWorld serverWorld) {
-                    ItemStack repairedArrow = repair(fletchingTable, stack, player);
-                    player.setStackInHand(hand, repairedArrow);
+                    repair(world, pos, stack);
 
                     if (MarTweaks.CONFIG.arrowRecovery.rewardRepairingBrokenArrow()) {
                         ExperienceOrbEntity.spawn(serverWorld, pos.up().toBottomCenterPos(),
@@ -55,8 +56,9 @@ public class BrokenArrowItem extends Item {
         return super.useOnBlock(context);
     }
 
-    protected ItemStack repair(Block fletchingTable, ItemStack stack, PlayerEntity player) {
-        player.incrementStat(Stats.USED.getOrCreateStat(fletchingTable.asItem()));
-        return ItemUsage.exchangeStack(stack, player, Items.ARROW.getDefaultStack());
+    protected void repair(World world, BlockPos pos, ItemStack stack) {
+        stack.decrement(1);
+        ItemDispenserBehavior.spawnItem(world, Items.ARROW.getDefaultStack(), 2, Direction.UP,
+                Vec3d.ofBottomCenter(pos).offset(Direction.UP, 1.2));
     }
 }
